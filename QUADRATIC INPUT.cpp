7541c311833a1b612d06503ab228ae9dadd15f1a
IAmLaggingSoBad <118320879+IAmLaggingSoBad@users.noreply.github.com>
2024-09-04 13:29:32 +0800

    Update QUADRATIC INPUT.cpp

--- QUADRATIC INPUT.cpp
+++ QUADRATIC INPUT.cpp
@@ -18,11 +18,11 @@ int task(){
     if (discriminant > 0){
         double x1 = (-b + sqrt(discriminant)) / (2 * a);
         double x2 = (-b - sqrt(discriminant)) / (2 * a);
-        cout << "There are two distinct real roots, " << x1 << " and " << x2 << endl;
+        cout << "There are two distinct real roots, x1 = " << x1 << " and x2 = " << x2 << endl;
     }
     else if (discriminant == 0){
         double x = -b / (2 * a);
-        cout << "There is exactly one real root, " << x << endl;
+        cout << "There is exactly one real root, x = " << x << endl;
     }
     else{
         cout << "There are no real roots (the roots are complex).\n";
