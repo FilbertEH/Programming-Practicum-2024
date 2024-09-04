9dd3d7740e2dad4f3f4309cbc93d24cfe698aefa
IAmLaggingSoBad <118320879+IAmLaggingSoBad@users.noreply.github.com>
2024-09-04 13:07:24 +0800

    Update PAYSLIP INPUT.cpp

--- PAYSLIP INPUT.cpp
+++ PAYSLIP INPUT.cpp
@@ -10,20 +10,22 @@ long int getInput(string prompt){
 }
 
 string format(long int value) {
-    string original = to_string(value);
+    string original = to_string(value); 
     string format;
-    for (int i = 0; i < original.length(); i++) {
+    for (int i = 0; i < original.length(); i++) { 
         format += original[i];
         if (i != original.length() - 1 && (original.length() - i - 1) % 3 == 0) {
             format += '.';
         }
     }
-    return format + ",00"; 
+    return format + ",00";
 }
 
-void displayResult(string name, long int gross, long int tax, long int installment, long int insurance, long int net){
-    cout << "Payslip for Employee" << endl
-    << "---------------------" << endl 
+void displayResult(string name, long int gross, long int installment, long int insurance){
+    long int tax = gross * 0.2;
+    long int net = gross - tax - installment - insurance;
+    cout << "\n     Payslip for Employee\n"
+    << "------------------------------\n" 
     << "Name: " << name << endl
     << "Gross Salary: Rp" << format(gross) << endl
     << "Tax (20%)   : Rp" << format(tax) << endl
@@ -32,13 +34,25 @@ void displayResult(string name, long int gross, long int tax, long int installme
     << "Net Salary  : Rp" << format(net) << endl;
 }
 
-int main(){
-    string name; cout << "Input Name: "; getline( cin, name);
+int task(string name){
     long int gross = getInput("Input Gross Salary: ");
-    long int tax = gross * 0.2;
     long int installment = getInput("Input Installment: ");
     long int insurance = getInput("Input Insurance: ");
-    long int net = gross - tax - installment - insurance;
-    displayResult(name, gross, tax, installment, insurance, net);
+    displayResult(name, gross, installment, insurance);
+    return 0;
+}
+
+int main(){
+    char again = 'y';
+    do {
+        string name;
+        cout << "------------------------------\nInput Name: ";
+        while (name == "") {
+            getline(cin, name);
+        }
+        task(name);
+        cout << "\nWould you like to start again [y/n]: ";
+        cin >> again;
+    } while (again == 'y');
     return 0;
 }
