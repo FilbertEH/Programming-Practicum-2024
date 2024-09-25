125f6660757dc000802ee6cd21ed7b4ea9a55ae4
Filbert Ethan H. <filbertethanh@gmail.com>
2024-09-25 09:44:33 +0800

    Update problem_1_input.cpp

--- week_2/problem_1_input.cpp
+++ week_2/problem_1_input.cpp
@@ -2,42 +2,42 @@
 #include <string>
 using namespace std;
 
-long int getInput(string prompt){
-    long int value;
+long getInput(string prompt){
+    long value;
     cout << prompt;
     cin >> value;
     return value;
 }
 
-string format(long int value) {
+string format(long value) {
     string original = to_string(value); 
     string format;
-    for (int i = 0; i < original.length(); i++) { 
+    for (int i = 0; i < original.size(); i++) { 
         format += original[i];
-        if (i != original.length() - 1 && (original.length() - i - 1) % 3 == 0) {
+        if (i != original.length() - 1 && (original.size() - i - 1) % 3 == 0) {
             format += '.';
         }
     }
-    return format + ",00";
+    return "Rp" + format + ",00";
 }
 
-void displayResult(string name, long int gross, long int installment, long int insurance){
-    long int tax = gross * 0.2;
-    long int net = gross - tax - installment - insurance;
+void displayResult(string name, long gross, long installment, long insurance){
+    long tax = gross * 0.2;
+    long net = gross - tax - installment - insurance;
     cout << "\n     Payslip for Employee\n"
     << "------------------------------\n" 
     << "Name: " << name << endl
-    << "Gross Salary: Rp" << format(gross) << endl
-    << "Tax (20%)   : Rp" << format(tax) << endl
-    << "Installment : Rp" << format(installment) << endl
-    << "Insurenace  : Rp" << format(insurance) << endl
-    << "Net Salary  : Rp" << format(net) << endl;
+    << "Gross Salary: " << format(gross) << endl
+    << "Tax (20%)   : " << format(tax) << endl
+    << "Installment : " << format(installment) << endl
+    << "Insurenace  : " << format(insurance) << endl
+    << "Net Salary  : " << format(net) << endl;
 }
 
 int task(string name){
-    long int gross = getInput("Input Gross Salary: ");
-    long int installment = getInput("Input Installment: ");
-    long int insurance = getInput("Input Insurance: ");
+    long gross = getInput("Input Gross Salary: ");
+    long installment = getInput("Input Installment: ");
+    long insurance = getInput("Input Insurance: ");
     displayResult(name, gross, installment, insurance);
     return 0;
 }
