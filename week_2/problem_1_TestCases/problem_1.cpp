b2b7df070c95c3ce84980d37c6a0a06a16617e28
Filbert Ethan H. <filbertethanh@gmail.com>
2024-09-25 09:38:23 +0800

    Update problem_1.cpp

--- week_2/problem_1_TestCases/problem_1.cpp
+++ week_2/problem_1_TestCases/problem_1.cpp
@@ -3,30 +3,30 @@
 #include <vector>
 #include <string>
 
-std::string format(long int value) {
+std::string format(long value) {
     std::string original = std::to_string(value);
     std::string format;
-    for (int i = 0; i < original.length(); i++) {
+    for (int i = 0; i < original.size(); i++) {
         format += original[i];
-        if (i != original.length() - 1 && (original.length() - i - 1) % 3 == 0) {
+        if (i != original.size() - 1 && (original.size() - i - 1) % 3 == 0) {
             format += '.';
         }
     }
-    return format + ",00"; 
+    return "Rp" + format + ",00"; 
 }
 
-std::vector<std::string> payslip(std::string name, long int gross, long int installment, long int insurance){
+std::vector<std::string> payslip(std::string name, long gross, long installment, long insurance){
     std::vector<std::string> payslip;
-    long int tax = gross * 0.2;
-    long int net = gross - tax - installment - insurance;
+    long tax = gross * 0.2;
+    long net = gross - tax - installment - insurance;
     payslip.push_back("Payslip for Employee");
     payslip.push_back("--------------------");
     payslip.push_back("Name:" + name);
-    payslip.push_back("Gross Salary: Rp" + format(gross));
-    payslip.push_back("Tax (20%)   : Rp" + format(tax));
-    payslip.push_back("Installment : Rp" + format(installment));
-    payslip.push_back("Insurance   : Rp" + format(insurance));
-    payslip.push_back("Net Salary  : Rp" + format(net));
+    payslip.push_back("Gross Salary: " + format(gross));
+    payslip.push_back("Tax (20%)   : " + format(tax));
+    payslip.push_back("Installment : " + format(installment));
+    payslip.push_back("Insurance   : " + format(insurance));
+    payslip.push_back("Net Salary  : " + format(net));
     return payslip;
 }
 
@@ -43,7 +43,7 @@ int main(){
     int i = 1;
     while (getline(testFile, line)) {
         std::string name;
-        long int gross, installment, insurance;
+        long gross, installment, insurance;
         std::vector<std::string> expected_output, output;
         getline(testFile, line); 
         name = line.substr(line.find(":") + 1);
@@ -74,11 +74,11 @@ int main(){
         if (output == expected_output) {
             std::cout << "\n----- TEST CASE " << i << " PASSED [OUTPUT MATCH] -----\n" <<
             "Input:\n\nName:" << name << "\nGross: " << gross << "\nInstallment: " << installment << "\nInsurance: " << insurance <<
-            "\n____________________\n" << genExpect << "\n____________________\n" << genOutput;
+            "\n____________________\n" << genExpect << "____________________\n" << genOutput;
         } else {
             std::cout << "\n----- TEST CASE " << i << " FAILED [IRREGULARITY FOUND] -----\n" << 
             "Input:\n\nName:" << name << "\nGross: " << gross << "\nInstallment: " << installment << "\nInsurance: " << insurance <<
-            "\n____________________\n" << genExpect << "\n____________________\n" << genOutput;
+            "\n____________________\n" << genExpect << "____________________\n" << genOutput;
         }
         i++;
     }
