725d375992f4d3a5b8be81c344c424f438f8eb05
Filbert Ethan H. <filbertethanh@gmail.com>
2024-09-26 19:46:58 +0700

    Update problem_2.cpp

--- week_3/problem_2/problem_2.cpp
+++ week_3/problem_2/problem_2.cpp
@@ -1,31 +1,62 @@
 #include <cmath>
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-double getInput(string prompt){
-    double value;
-    cout << prompt;
-    cin >> value;
-    return value;
+int task(int a, int b, int c){
+    int large = a;
+    if (large < b){
+        large = b;
+    }
+    if (large < c){
+        large = c;
+    }
+    return large;
 }
 
 int main(){
-    char again = 'y';
-    cout << "Finding the largest number between three numbers:\n";
-    do{
-        double a = getInput("Input number 1: ");
-        double b = getInput("Input number 2: ");
-        double c = getInput("Input number 3: ");
-        int large = a;
-        if (large < b){
-            large = b;
+    std::cout << "Largest Number Among Three Numbers\n";
+    std::ifstream testFile("test.txt");
+    std::string line;
+    if (!testFile) {
+        std::cerr << "Error: Could not open test.txt" << std::endl;
+        return 1;
+    }
+
+    int i = 1;
+    while (getline(testFile, line)) {
+        std::vector<double> input;
+        std::stringstream ss(line.substr(line.find(":") + 1)); 
+        std::string temp;
+        while (getline(ss, temp, ',')){
+            input.push_back(std::stoi(temp));
         }
-        if (large < c){
-            large = c;
+
+        int a = input[0];
+        int b = input[1];
+        int c = input[2];
+        int output = task(a, b, c);
+
+        int expected;
+        getline(testFile, line);
+        ss.clear();
+        ss.str(line.substr(line.find(":") + 1));
+        ss >> expected;
+
+        if (output == expected) {
+            std::cout << "----- TEST CASE " << i << " PASSED [OUTPUT MATCH] -----\n" <<
+            "Input: " << a << ", " << b << ", " << c << 
+            "\nExpected Output: " << expected << 
+            "\nProgram Output:  " << output << std::endl;
+        } else {
+            std::cout << "----- TEST CASE " << i << " FAILED [IRREGULARITY FOUND] -----\n" <<
+            "Input: " << a << ", " << b << ", " << c << 
+            "\nExpected Output: " << expected << 
+            "\nProgram Output:  " << output << std::endl;
         }
-        cout << "The largest number is: " << large << endl;
-        cout << "\nTry again? [y/n]: ";
-        cin >> again;
-    }while (again == 'y');
+        i++;
+    }
     return 0;
 }
