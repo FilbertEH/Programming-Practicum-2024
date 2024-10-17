1be011216e7c6c56943c7f68e7df448873b50db6
IAmLaggingSoBad <iamlaggingsobad@gmail.com>
2024-10-17 08:28:53 +0700

    Update problem_1.cpp

---
+++ week_6/problem_1/problem_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+struct studentData {
+    int nim;
+    int UTS;
+    int UAS;
+};
+
+int task(int UTS, int UAS){
+
+}
